adcd26d98e16a83df6933f9001506df8b35a1cec
Gavin123 <gaywang@ucsc.edu>
2025-02-27 17:19:16 -0800

    finalized data set, cleaned

--- AirplaneClassify.py
+++ AirplaneClassify.py
@@ -5,6 +5,7 @@ import torch
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import math
+from PIL import Image
 from hidden import dataset_path, testTXT
 
 
@@ -30,4 +31,23 @@ df = df.join(df_test_manufacturer)
 print(df.head()) 
 
 
+#removing the image/plane id from plane type colum
+df['plane type'] = df["plane type"].apply(lambda x:x[1:])
+df['plane type'] = df["plane type"].apply(lambda x:"".join(x))
+print(df['plane type'])
+
+df = df.drop(columns=["Classes", "Labels"]) #remove unnecessary coloums 
+
+
+print(df.head())
+
+#########DISPLAY IMAGES############
+# for i in range(1, 26):
+#     plt.subplot(5, 5, i)
+#     plt.imshow(Image.open(f"/Users/gavinwang/Desktop/CMPM17_ML/CMPM-17-Final-Airplane-Classification/Final Project Data/archive/fgvc-aircraft-2013b/fgvc-aircraft-2013b/data/images/{df.loc[i, "filename"]}"))
+    
+#     plt.axis("off")
+#     plt.title(df.loc[i, "plane type"])
+# plt.tight_layout()
+# plt.show()
 
